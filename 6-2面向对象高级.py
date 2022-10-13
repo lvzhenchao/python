82064571f04070c978059820329b3758ea3f718e
lvzhenchao <1370164516@qq.com>
2022-10-13 11:17:10 +0800

    Update 6-2面向对象高级.py

--- 6-2面向对象高级.py
+++ 6-2面向对象高级.py
@@ -138,21 +138,36 @@ bat.fly()
 
 # 定制类：类似__slots__这种形如__xxx__的变量或者函数名就要注意，这些在Python中是有特殊用途的
 ## __slots__是为了控制类的属性，__len__方法为了让class作用于len()函数
-### __str__：特殊打印控制；打印出来的实例，不但好看，而且容易看出实例内部重要的数据
+## __str__：特殊打印控制；打印出来的实例，不但好看，而且容易看出实例内部重要的数据
 class Student3(object):
 	def __init__(self, name):
 		self.name = name
 	def __str__(self):
 		return 'Student object (name: %s)' % self.name
 	__repr__ = __str__# 增加一个这个就可以调试偷懒模式
+	
 print(Student3('lzc'))# <__main__.Student3 object at 0x0000016F308638B0>,打印出的不好看
 ### 增加了__str__之后，打印的就比较好看了
 
 s3 = Student3("lzc123")
 print(s3)
-## __str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串，也就是__repr__是为调试服务的
-## __repr__ = __str__
+### __str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串，也就是__repr__是为调试服务的
+### __repr__ = __str__；通常__str__()和__repr__()代码都是一样的，所以，有个偷懒的写法
 
+## __iter__()方法，返回一个迭代对象；python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，知道遇到StopIteration错误时退出循环
+class Fib(object):
+	def __init__(self):
+		self.a, self.b = 0, 1# 初始化两个计数器a, b
+	def __iter__(self):
+		return self # 实例本身就是迭代对象，故返回自己
+	def __next__(self):
+		self.a, self.b = self.b, self.a + self.b 
+		if self.a > 10000:
+			raise StopIteration()
+		return self.a
+		
+for n in Fib():
+	print(n)
 
 
 
