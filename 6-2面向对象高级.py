042daf6ac64d55a0a874a053f82c07322ef9ea9c
lvzhenchao <1370164516@qq.com>
2022-10-14 17:18:56 +0800

    Update 6-2面向对象高级.py

--- 6-2面向对象高级.py
+++ 6-2面向对象高级.py
@@ -248,6 +248,22 @@ class Weekday(Enum):
     Fri = 5
     Sat = 6
 print(Weekday.Mon)
+
+# 元类
+print(type(Student5))
+print(type(s5))
+## type()函数既可以返回一个对象的类型，又可以创建出新的类型，需要传入3个参数
+### 1、class的名称；
+### 2、继承的父类集合，注意Python支持多重继承，如果只有一个父类，别忘了tuple的单元素写法；
+### 3、class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello上。
+def fn(self, name="world"): #先定义函数
+	print("Hello, %s." % name)
+Hello = type('Hello', (object,), dict(hello=fn)) # 创建Hello class
+h = Hello()
+h.hello()
+print(type(Hello))
+print(type(h))
+
 	
 
 
