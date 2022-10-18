1e1d6dbafeab37e5073be959c1888cd015017d8d
lvzhenchao <1370164516@qq.com>
2022-10-18 14:28:24 +0800

    Update 7、错误-调试-测试.py

--- 7、错误-调试-测试.py
+++ 7、错误-调试-测试.py
@@ -19,8 +19,72 @@ except ValueError as e:
     print('ValueError:', e)
 except ZeroDivisionError as e:
     print('ZeroDivisionError:', e)
-else:
+else: # 当没有错误发生时，会自动执行else语句
 	print("没毛病")
 finally:
     print('finally...')
 print('END')
+
+# 记录错误：内置的logging模块可以非常容易地记录错误信息
+import logging
+def foo(s):
+	return 10 / int(s)
+def bar(s):
+	return foo(s) * 2
+def main():
+	try:
+		bar("1")
+	except Exception as e:
+		logging.exception(e)
+main()
+print("loggin 测试完毕")
+
+## raise错误
+def foo1(s):
+	n = int(s)
+	if n == 0:
+		raise ValueError("非法数据")
+	return 10 / n
+foo1(3)
+
+## 添加到try
+def bar1():
+	try:
+		foo1(0)
+	except ValueError as e:
+		print("ValueError!!!")
+		raise
+bar1()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
